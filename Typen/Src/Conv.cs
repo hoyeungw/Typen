34543099f30411dfc7828a51f48e68104153b60d
hoyeungw <hoyeungw@outlook.com>
2021-01-26 08:47:12 +0800

    add structures

---
+++ Typen/Src/Conv.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Typen {
+  public static class Conv {
+    public static TO Cast<T, TO>(T some) {
+      if (some is TO value) return value;
+      try { return (TO) Convert.ChangeType(some, typeof(T)); }
+      catch (InvalidCastException) { return default(TO); }
+    }
+
+    public static string ToStr<T>(T some) {
+      if (some is string str) return str;
+      return some.ToString();
+    }
+  }
+  
+  
+}
