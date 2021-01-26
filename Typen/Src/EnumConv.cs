6ac5c33946f2d244da818f5d818f8c98fda4f8f2
hoyeungw <hoyeungw@outlook.com>
2021-01-26 09:12:52 +0800

    add Typen.Test

--- Typen/Src/EnumConv.cs
+++ Typen/Src/EnumConv.cs
@@ -1,32 +1,29 @@
-﻿// using System;
-//
-// namespace Typen {
-//   public static class EnumConv<TEnum> {
-//     /// <summary>
-//     /// 字符串转换为Enum
-//     /// </summary>
-//     /// <returns></returns>
-//     public static string[] Names => Enum.GetNames(typeof(TEnum));
-//     public static int[] Values {
-//       get {
-//         var oAr = Enum.GetValues(typeof(TEnum));
-//         var tAr = new int[oAr.GetLength(0)];
-//         oAr.CopyTo(tAr, 0);
-//         return tAr;
-//       }
-//     }
-//     public static string ToText(TEnum enumItem) {
-//       var τ = Enum.GetName(typeof(TEnum), enumItem);
-//       τ = τ.Leakage();
-//       return τ;
-//     }
-//   }
-//
-//   public abstract class EnumExt {
-//     public static string ToText<TEnum>(TEnum enumItem) {
-//       var τ = Enum.GetName(typeof(TEnum), enumItem);
-//       τ = τ.Leakage();
-//       return τ;
-//     }
-//   }
-// }
+﻿using System;
+
+namespace Typen {
+  public static class EnumConv<TEnum> {
+    /// <summary>
+    /// 字符串转换为Enum
+    /// </summary>
+    /// <returns></returns>
+    public static string[] Names => Enum.GetNames(typeof(TEnum));
+    public static int[] Values {
+      get {
+        // return Array.ConvertAll(Enum.GetValues(typeof(TEnum)), x => (int) x);
+
+        // var array = Enum.GetValues(typeof(TEnum));
+        // var vector = new int[array.Length];
+        // array.CopyTo(vector, 0);
+        // return vector;
+
+        return (int[]) Enum.GetValues(typeof(TEnum));
+        typeof(TEnum).GetEnumValues();
+      }
+    }
+    public static string ToText(TEnum item) => Enum.GetName(typeof(TEnum), item);
+  }
+
+  public abstract class EnumExt {
+    public static string ToText<TEnum>(TEnum item) => Enum.GetName(typeof(TEnum), item);
+  }
+}
