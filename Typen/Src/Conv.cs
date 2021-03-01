82aa7a42b77cde2318422555af3dde4d07482bd3
hazen <hoyeungw@outlook.com>
2021-03-01 14:17:51 +0800

    minor fix

--- Typen/Src/Conv.cs
+++ Typen/Src/Conv.cs
@@ -7,13 +7,11 @@ namespace Typen {
       try { return (TO) Convert.ChangeType(some, typeof(TO)); }
       catch (InvalidCastException) { return default(TO); }
     }
-
     public static TO Cast<T, TO>(T some, TO def) {
       if (some is TO value) return value;
       try { return (TO) Convert.ChangeType(some, typeof(TO)); }
       catch (InvalidCastException) { return def; }
     }
-
     public static string ToStr<T>(T some) => some is string str ? str : some.ToString();
   }
 }
