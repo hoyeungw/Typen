9e138f2e684bf2db61bec16083843f70eb089452
hoyeungw <hoyeungw@outlook.com>
2021-08-13 18:04:22 +0800

    add Typen.Enum

---
+++ Typen.Enum/Src/Enum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Typen {
+  public static class Enum<TEnum> {
+    public static string[] Names => System.Enum.GetNames(typeof(TEnum));
+    public static Array Values => System.Enum.GetValues(typeof(TEnum));
+    public static int[] IntValues => (int[]) System.Enum.GetValues(typeof(TEnum));
+    public static string Label(TEnum item) => System.Enum.GetName(typeof(TEnum), item);
+  }
+
+  public static class Enum {
+    public static string Label<TEnum>(TEnum item) => System.Enum.GetName(typeof(TEnum), item);
+    public static TEnum ToEnum<TEnum>(this string text) => (TEnum) System.Enum.Parse(
+      typeof(TEnum),
+      text.Replace(" ", ""),
+      true
+    );
+  }
+}
